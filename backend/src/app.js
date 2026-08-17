e8e5796cfcf78eaafcbe368c71fc329bdb1c372f
Youssef Rhouma <youssefrhouma316@gmail.com>
2026-08-17 11:51:27 +0200

    connection database

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const cors = require('cors');
+import express from 'express';
+import cors from 'cors';
 
 const app = express();
 
@@ -12,4 +12,4 @@ app.get("/", (req, res) => {
   });
 });
 
-module.exports = app;
+export default app;
--- backend/src/config/databse.js
+++ backend/src/config/databse.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
 
 const connectDB = async () => {
   try {
@@ -11,4 +13,4 @@ const connectDB = async () => {
   }
 };
 
-module.exports = connectDB;
+export default connectDB;
